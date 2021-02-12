6202f0f6fcba29a71d6988f295b9fa8a3ea18aca
Parvathy <you@example.com>
2021-02-12 13:10:35 +0530

    Amazon app

---
+++ amazon_backend/data.js
@@ -0,0 +1,80 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Fastrack Watch Band',
+            category: 'Watches',
+            image: '/images/1.jpg',
+            countInStock: 10,
+            price: 1600,
+            brand: 'Fastrack',
+        },
+        {
+            _id: '2',
+            name: 'Shoulder Bag Combo',
+            category: 'Bag',
+            image: '/images/4.jpg',
+            countInStock: 10,
+            price: 699,
+            brand: 'Puma',
+        },
+        {
+            _id: '3',
+            name: 'Libas Best Kurti',
+            category: 'Kurti',
+            image: '/images/3.jpg',
+            countInStock: 10,
+            price: 70,
+            brand: 'Libas',
+        },
+        {
+            _id: '4',
+            name: 'Allensolly Shirts for Men',
+            category: 'Shirt',
+            image: '/images/6.jpg',
+            countInStock: 10,
+            price: 2399,
+            brand: 'Peter England',
+        },
+        {
+            _id: '5',
+            name: 'Saree for Women',
+            category: 'Saree',
+            image: '/images/2.jpg',
+            countInStock: 0,
+            price: 999,
+            brand: 'Libas',
+        },
+        {
+            _id: '6',
+            name: 'Best Shoes',
+            category: 'Shoes',
+            image: '/images/5.jpg',
+            countInStock: 1,
+            price: 500,
+            brand: ' Puma',
+        },
+        {
+            _id: '7',
+            name: 'Kashvi Regular Fit Dungaree',
+            category: 'Dress',
+            image: '/images/7.jpg',
+            countInStock: 1,
+            price: 390,
+            brand: ' Kashvi',
+        },
+        {
+            _id: '8',
+            name: 'New Apple iPhone 11',
+            category: 'Shoes',
+            image: '/images/8.jpg',
+            countInStock: 1,
+            price: 45500,
+            brand: ' Apple',
+        },
+
+    ]
+}
+
+
+export default data;
